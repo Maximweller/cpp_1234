78936c3b50d11493f4be7a25a244b3670ab3d568
Maximweller <38182511+Maximweller@users.noreply.github.com>
2019-12-01 03:07:14 +0300

    Add files via upload

---
+++ task2(percent)/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	const double Anna_percent = 0.10;
+	const double Boris_percent = 0.05;
+
+	double Anna, Boris, Anna_profit;
+	int i;
+
+	cout << "Введите исходный баланс Анны: ";
+	cin >> Anna;
+	cout << "Введите исходный баланс Бориса: ";
+	cin >> Boris;
+
+	if ((Boris < 0) || (Anna < 0)) {
+		cout << "Вы ввели отрицательное значение баланса!" << endl;
+		system("pause");
+		exit(0);
+	}
+	else if ((Boris == 0) || (Anna == 0)) {
+		cout << "Вы ввели нулевой баланс!" << endl;
+		system("pause");
+		exit(0);
+	}
+	else if (Boris > Anna) {
+		cout << "Баланс Бориса и так больше чем баланс Анны" << endl;
+		system("pause");
+		exit(0);
+	}
+
+	Anna_profit = Anna * Anna_percent;
+
+	cout << "\n\n Год\t" << "\tБаланс Анны\t\tБаланс Бориса\n" << endl;
+	for (i = 1; Boris <= Anna; ++i) {
+		Anna += Anna_profit;
+		Boris += Boris * Boris_percent;
+		cout << " " << i << "\t\t" << Anna << "\t\t\t" << Boris << endl;
+	}
+
+	cout << "\n\n" << i - 1 << " лет необходимо, чтобы сумма баланса Бориса превысила сумму баланса Анны." << endl;
+	system("pause");
+	return 0;
+}
---
+++ task3(tax)/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	const double tax1 = 0.00;
+	const double tax2 = 0.10;
+	const double tax3 = 0.15;
+	const double tax4 = 0.20;
+
+	double ebit, net;
+
+	ebit = 0;
+
+	cout << "Ваш личный помощник в подсчете налогов.\n" << endl;
+
+	while (ebit >= 0)
+	{
+		cout << "\nВведите доход: ";
+		cin >> ebit;
+
+		net = ebit * tax1;
+
+		if (ebit > 35000) {
+			double rest = ebit - 35000;
+			net += 10000 * tax2 + 20000 * tax3 + rest * tax4;
+		}
+		else if (ebit >= 15000) {
+			double rest = ebit - 15000;
+			net += 10000 * tax2 + rest * tax3;
+		}
+		else if (ebit >= 5000) {
+			double rest = ebit - 5000;
+			net += rest * tax2;
+		}
+
+		if (ebit >= 0) {
+			cout << "\nСумма налога: " << net << " рублей;" << endl;
+			if (ebit > 0) {
+				cout << "Что составляет " << (net * 100) / ebit << " процент от вашего дохода;" << endl;
+			}
+			else if (ebit == 0) {
+				cout << "Что составляет: 0" << " процентов от вашего дохода;" << endl;
+			}
+			cout << "Чистая прибыль: " << ebit - net << " рублей." << endl;
+		}
+	}
+
+	cout << "\nВы ввели отрицательный доход!" << endl;
+	system("pause");
+	return 0;
+}
