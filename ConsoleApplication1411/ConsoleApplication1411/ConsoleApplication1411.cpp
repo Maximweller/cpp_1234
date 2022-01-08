6fe8a988ec7ca467342fed02a7590ad04eaad0ab
Guseyn Salimov <maximweller@protonmail.com>
2022-01-08 15:02:24 +0300

    lessons, weekly hw

---
+++ ConsoleApplication1411/ConsoleApplication1411/ConsoleApplication1411.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+
+/*
+struct str {
+	double w;
+	float v;
+};
+
+
+struct myStruct {
+	int a;
+	char c;
+	str s;
+
+};
+
+int main(int argc, char** argv)
+{
+	myStruct q;
+	q.a = 34;
+	q.c = 'r';
+	q.s.v = 45.0f;
+	q.s.w = 6.22;
+
+	cout << sizeof(q) << " " << sizeof(myStruct) << endl;
+
+	system("pause");
+	return 0;
+};*/
+
+
+/*
+int main()
+{
+	int a = 3;
+
+	char c = (a % 2) ? 'o' : 'e';
+	cout << c << endl;
+	cout << "" << endl;
+
+
+	for (int i = 0, j = 10 ; i <= 10 ; i += 2, j -= 1)
+	{
+		cout << "i = " << i << ", j = " << j << endl;
+	}
+
+	cout << "" << endl;
+
+	switch (a) {
+	case 1:
+		cout << "1" << endl;
+	case 3:
+		cout << "3" << endl;
+	case 5:
+		cout << "5" << endl;
+		break;
+	case 7:
+		cout << "7" << endl;
+	}
+
+	cout << "" << endl;
+
+
+
+
+	system("pause");
+	return 0;
+};*/
---
+++ current1(factorial)/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int n = 1;
+
+	while (n >= 0) {
+		cout << "Введите число: ";
+		cin >> n;
+		double k = 1;
+		
+		for (int i = 1; i != n + 1; ++i) {
+			k = k * i;
+		}
+		cout << "Факториал = " << k << "\n" << endl;
+	}
+	system("pause");
+	return 0;
+}
